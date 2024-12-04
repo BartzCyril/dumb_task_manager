0eff8ea2f1cce0445b2ff771496188d87217fdb0
Pierre Braem <pierrebraem@orange.fr>
2024-12-04 16:26:37 +0100

    refacto: mise à jour route auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,8 @@ const users = require('../models/user');
 const bcrypt = require('bcrypt');
 
 // Placeholder routes for authentication
-router.get('/login', (req, res) => res.render('login', { user: undefined }));
-router.post('/login', (req, res) => {
+router.get('/auth/login', (req, res) => res.render('login', { user: undefined }));
+router.post('/auth/login', (req, res) => {
     users.authenticate(req.body.username, req.body.password, (err, user) => {
         if(err) {
             res.status(500).send(`Une erreur est survenue lors de la connexion ${err.message}`);
@@ -20,7 +20,7 @@ router.post('/login', (req, res) => {
     })
 })
 
-router.get('/user/register', (req, res) => {
+router.post('/user/register', (req, res) => {
     const username = req.query.username;
     const password = req.query.password;
     const email = req.query.email;
@@ -77,6 +77,6 @@ router.get('/user/register', (req, res) => {
         });
     })
 })
-router.get('/register', (req, res) => res.render('register',{ user: undefined }));
+router.get('/auth/register', (req, res) => res.render('register',{ user: undefined }));
 
 module.exports = router;
