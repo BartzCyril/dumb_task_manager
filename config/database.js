d0c18fdcb2c1cb85d6a0efc3339ccd6a31077d5c
Pierre Braem <pierrebraem@orange.fr>
2024-12-05 15:55:27 +0100

    tests: mise en place du mock

--- config/database.js
+++ config/database.js
@@ -1,8 +1,17 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path')
 
+let chemin = "";
+
+if(process.env.NODE_ENV == 'test'){
+    chemin = ":memory:"
+}
+else{
+    chemin = path.join(__dirname, './tasks.sqlite')
+}
+
 // Connexion à la base de données SQLite
-const db = new sqlite3.Database(path.join(__dirname, './tasks.sqlite'), (err) => {
+const db = new sqlite3.Database(chemin, (err) => {
     if (err) {
         console.error('Erreur lors de la connexion à la base de données:', err.message);
     } else {
