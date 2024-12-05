52c4c1f686a31321e67f2b6ed9d232d1f7ba575e
Pierre Braem <pierrebraem@orange.fr>
2024-12-05 20:21:13 +0100

    test: adaptation des tests en fonction de "is_admin"

--- config/seed-memory-database.js
+++ config/seed-memory-database.js
@@ -3,9 +3,10 @@ const db = require('./database.js')
 function seedDatabase() {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
-            db.run('CREATE TABLE users (id INT, username TEXT, password TEXT, email TEXT)');
-            db.run('INSERT INTO users (id, username, password, email) VALUES  (1, "user1", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user1@example.com")')
-            db.run('INSERT INTO users (id, username, password, email) VALUES  (2, "user2", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user2@example.com")', 
+            db.run('CREATE TABLE users (id INT, username TEXT, password TEXT, email TEXT, is_admin BOOL)');
+            db.run('INSERT INTO users (id, username, password, email, is_admin) VALUES (1, "admin", "$2b$10$DA7ohjIhQtxpuo1wlN5KvetPXArmr/VvwvpIqaKZdhoS.zhTMPkre", "admin@example.com", 1)')
+            db.run('INSERT INTO users (id, username, password, email, is_admin) VALUES (2, "user1", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user1@example.com", 0)')
+            db.run('INSERT INTO users (id, username, password, email, is_admin) VALUES (3, "user2", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user2@example.com", 0)', 
                 (err) => {
                     if(err) {
                         reject(err)
