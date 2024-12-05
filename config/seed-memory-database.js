17cc55d0e49cd58c59824a8632174e35f3f8b2d2
Pierre Braem <64416044+pierrebraem@users.noreply.github.com>
2024-12-05 20:59:13 +0100

    Implémentation des tests pour le login (#15)
    * test: impplémentation tests unitaire login
    
    * test: implémentation test fonctionnel login
    
    * fix: Mot de passe pour user 1 et 2 identique
    
    ---------
    
    Co-authored-by: Cyril BARTZ <123480754+BartzCyril@users.noreply.github.com>

--- config/seed-memory-database.js
+++ config/seed-memory-database.js
@@ -6,7 +6,7 @@ function seedDatabase() {
             db.run('CREATE TABLE users (id INT, username TEXT, password TEXT, email TEXT, is_admin BOOL)');
             db.run('INSERT INTO users (id, username, password, email, is_admin) VALUES (1, "admin", "$2b$10$DA7ohjIhQtxpuo1wlN5KvetPXArmr/VvwvpIqaKZdhoS.zhTMPkre", "admin@example.com", 1)')
             db.run('INSERT INTO users (id, username, password, email, is_admin) VALUES (2, "user1", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user1@example.com", 0)')
-            db.run('INSERT INTO users (id, username, password, email, is_admin) VALUES (3, "user2", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user2@example.com", 0)', 
+            db.run('INSERT INTO users (id, username, password, email, is_admin) VALUES (3, "user2", "$2b$10$XLfS/bMt0IckfRg14csICek7LPHGrJov/Yde/f5Y7eTMlvfikIWhu", "user2@example.com", 0)', 
                 (err) => {
                     if(err) {
                         reject(err)
