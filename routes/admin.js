0938f6be12dd897190d12e9015ce6c4fa081ba8c
Cyril BARTZ <123480754+BartzCyril@users.noreply.github.com>
2024-12-05 17:17:12 +0100

    refacto: page admin en front + ajout d'une fonction pour supprimer l'utilisateur

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,30 @@ const express = require('express');
 const router = express.Router();
 const users = require('../models/user');
 
-
 router.get('/', (req, res) => {
     users.getAllUsers((err, users) => {
         if(err){
-            res.status(500).send(`Une erreur est survenue lors de la récupération des utilisateurs ${err.message}`);
+            res.status(500).send({message : `Une erreur est survenue lors de la récupération des utilisateurs ${err.message}`});
             return;
         }
-        res.render('admin', { users });
+        res.render('admin', { users, user: undefined });
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!id) {
+        res.status(400).send({message: 'Id is required'});
+        return;
+    }
+
+    users.deleteUser(id, (err, result) => {
+        if(err){
+            res.status(500).send({message: `Une erreur est survenue lors de la suppression de l'utilisateur ${err.message}`});
+            return;
+        }
+        res.status(200).send({message: `Utilisateur #${id} supprimé avec succès`});
     })
 })
 
