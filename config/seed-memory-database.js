1d09b2757d08bd72706865dfcf376afc765e4856
Pierre Braem <pierrebraem@orange.fr>
2024-12-05 20:46:19 +0100

    fix: Mot de passe pour user 1 et 2 identique

--- config/seed-memory-database.js
+++ config/seed-memory-database.js
@@ -5,7 +5,7 @@ function seedDatabase() {
         db.serialize(() => {
             db.run('CREATE TABLE users (id INT, username TEXT, password TEXT, email TEXT)');
             db.run('INSERT INTO users (id, username, password, email) VALUES  (1, "user1", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user1@example.com")')
-            db.run('INSERT INTO users (id, username, password, email) VALUES  (2, "user2", "$2b$10$hzw1pCfJL2hjakDvbQiDF.yzFvChm0R.XKGm5T/0BW7v5.yk3Lndi", "user2@example.com")', 
+            db.run('INSERT INTO users (id, username, password, email) VALUES  (2, "user2", "$2b$10$XLfS/bMt0IckfRg14csICek7LPHGrJov/Yde/f5Y7eTMlvfikIWhu", "user2@example.com")', 
                 (err) => {
                     if(err) {
                         reject(err)
