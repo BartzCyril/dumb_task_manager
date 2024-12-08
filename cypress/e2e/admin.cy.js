50bc8c18458c0c488154f416d3052cc205ba7290
Cyril BARTZ <123480754+BartzCyril@users.noreply.github.com>
2024-12-08 21:13:23 +0100

    fix: test e2e admin (#37)

--- cypress/e2e/admin.cy.js
+++ cypress/e2e/admin.cy.js
@@ -1,40 +1,52 @@
 describe('test admin', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000')
-        cy.get('.nav-link').contains('Login').click();
+    describe('tests with beforeEach', () => {
+        beforeEach(() => {
+            cy.visit('http://localhost:3000');
+            cy.get('.nav-link').contains('Login').click();
+        });
+
+        it('check if the "Administration" button exists for admin account', () => {
+            cy.get('input[name="username"]').type("admin1");
+            cy.get('input[name="password"]').type("Le#PetitChat1");
+
+            cy.get('button').contains('Login').click();
+            cy.url().should('eq', 'http://localhost:3000/');
+
+            cy.get('.nav-link').contains('Administration');
+        });
+
+        it('check if the "Administration" button doesn\'t exist for non-admin account', () => {
+            cy.get('input[name="username"]').type("cypress");
+            cy.get('input[name="password"]').type("Le#petitChat1");
+
+            cy.get('button').contains('Login').click();
+            cy.url().should('eq', 'http://localhost:3000/');
+
+            cy.get('.nav-link').should('not.contain', "Administration");
+        });
     });
 
-    it('check if the "Adminstration" button exists on for admin accout', () => {
-        cy.get('input[name="username"]').type("admin1")
-        cy.get('input[name="password"]').type("Le#PetitChat1");
+    describe('delete a user as administrator', () => {
+        before(() => {
+            cy.visit('http://localhost:3000');
+            cy.get('.nav-link').contains('Register').click();
+            cy.get('input[name="username"]').type("CypressTestAdmin");
+            cy.get('input[name="email"]').type("CypressTestAdmin@gmail.com");
+            cy.get('input[name="password"]').type("Le#PetitChat1");
+            cy.get('input[name="confirmPassword"]').type("Le#PetitChat1");
+            cy.get('button').contains('Register').click();
+            cy.get('.nav-link').contains('Login').click();
+            cy.get('input[name="username"]').type("admin1");
+            cy.get('input[name="password"]').type("Le#PetitChat1");
+            cy.get('button').contains('Login').click();
+            cy.url().should('eq', 'http://localhost:3000/');
+            cy.get('.nav-link').contains('Administration').click();
+        });
 
-        cy.get('button').contains('Login').click();
-        cy.url().should('eq', 'http://localhost:3000/');
-
-        cy.get('.nav-link').contains('Administration');
+        it('delete a user as administrator', () => {
+            cy.contains('tr', 'CypressTestAdmin').find('button').click();
+            cy.contains('button', 'Yes, Delete').click();
+            cy.contains('tr', 'CypressTestAdmin').should('not.exist');
+        });
     });
-
-    it('check if the "Adminstration" button doesn\'t exist for non-admin accout', () => {
-        cy.get('input[name="username"]').type("cypress")
-        cy.get('input[name="password"]').type("Le#petitChat1");
-
-        cy.get('button').contains('Login').click();
-        cy.url().should('eq', 'http://localhost:3000/');
-        
-        cy.get('.nav-link').should('not.contain', "Administration");
-    });
-
-    it('delete a user as administrator', () => {
-        cy.get('input[name="username"]').type("admin1");
-        cy.get('input[name="password"]').type("Le#PetitChat1");
-
-        cy.get('button').contains('Login').click();
-        cy.url().should('eq', 'http://localhost:3000/');
-
-        cy.get('.nav-link').contains('Administration').click();
-
-        cy.contains('tr', 'cypress').find('button').click();
-        cy.contains('button', 'Yes, Delete').click();
-        cy.contains('tr', 'cypress').should('not.exist');
-    })
-})
+});
